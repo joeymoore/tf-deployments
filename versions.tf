496db7a386fe45bff96e13d5e971b0550df0eba1
Joe Moore <joe.moore@imperva.com>
2022-05-13 08:32:04 -0700

    Downgrading provider to 2.8.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     incapsula = {
       source = "imperva/incapsula"
-      version = "2.9.0"
+      version = "2.8.0"
     }
   }
   required_version = ">= 0.13"
